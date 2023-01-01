8093a2c9bb85117c9d3c7a6645611bc994c33b66
Nidhish Yarlagadda <nidhish.yarlagadda@gmail.com>
2023-01-01 00:53:24 -0500

    updated dependencies to resolve merge conflicts

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Ionicons from "react-native-vector-icons/Ionicons";
 import SectionList from "react-native/Libraries/Lists/SectionList";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
 // Screens
 import HomeScreen from "./screens/HomeScreen";
 import TasksScreen from "./screens/TasksScreen";
@@ -37,18 +37,18 @@ function MainContainer() {
             } else if (rn === tasksName) {
               iconName = focused ? "list" : "list-outline";
             } else if (rn === rewardsName) {
-              iconName = focused ? 'trophy' : 'trophy-outline';
+              iconName = focused ? "trophy" : "trophy-outline";
             }
-          
-            return <Ionicons name={iconName} size={size} color={'white'} />;
+
+            return <Ionicons name={iconName} size={size} color={"white"} />;
           },
           headerTitle: () => <HeaderMoney />,
 
           // tabBarOptions:{
-            tabBarActiveTintColor: "white", 
-            tabBarInactiveTintColor: "grey",
-            tabBarLabelStyle: { paddingBottom: 20, fontSize: 20 },
-            tabBarStyle: {padding: 30, height: 70 , backgroundColor: '#5D2AA8'},
+          tabBarActiveTintColor: "white",
+          tabBarInactiveTintColor: "grey",
+          tabBarLabelStyle: { paddingBottom: 20, fontSize: 20 },
+          tabBarStyle: { padding: 30, height: 70, backgroundColor: "#5D2AA8" },
           // },
         })}
       >
@@ -56,7 +56,6 @@ function MainContainer() {
         <Tab.Screen name={tasksName} component={TasksScreen} />
         <Tab.Screen name={rewardsName} component={RewardsScreen} />
         {/* <Tab.Screen name={mapName} component={MapScreen} /> */}
-
       </Tab.Navigator>
     </NavigationContainer>
   );
