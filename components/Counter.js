a704296d5c5944256f66fae30fe19bdcaf6ee6bf
Richard Ahn <richardchadori@gmail.com>
2022-12-31 21:42:54 -0800

    Update to TaskScreen, implementing AddTask into it

--- components/Counter.js
+++ components/Counter.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react';
 import { View, StyleSheet, Text, Button } from 'react-native';
+
+import AddTask from './AddTask'
 const Counter = () => {
     const [count, setCount] = useState(0);
-    const [res, setRes] = useState(0);
+    const [res, setRes] = useState();
    return (
         <View>
            <Button
@@ -13,8 +15,11 @@ const Counter = () => {
              style = {styles.decrement}
              onPress={() => {setCount(count - 1)}} title="-"
            />
-           <Text color = '#014421'> {count} </Text>
-           
+           <Text color = '#E5DAF6'> {count} </Text>
+           <Button
+                style = {styles.nextButton}
+                onPress={() => this.props.navigation.goBack(<AddTask />)} title = "done"
+           />
        </View>
    );
 }
@@ -31,6 +36,11 @@ const styles = StyleSheet.create({
         flex: 0.5,
         backgroundColor: '#FFFFFF',
         flexDirection: 'row'
+    },
+    backButton: {
+        flex: 1.5,
+        backgroundColor: '#FFFFFF',
+        flexDirection: 'row-reverse'
     }
 })
 
